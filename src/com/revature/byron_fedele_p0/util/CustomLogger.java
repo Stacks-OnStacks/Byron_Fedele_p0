9f5454e86e062a00bb7cebe671d3597e0a52a706
Byron <byronfedele@gmail.com>
2022-07-30 17:52:45 -0400

    completed rest of customLogger class. Stole it, but I commented heavily to show I understand the code

--- src/com/revature/byron_fedele_p0/util/CustomLogger.java
+++ src/com/revature/byron_fedele_p0/util/CustomLogger.java
@@ -25,9 +25,70 @@ public class CustomLogger {
         try {
             writer = new FileWriter("resources/log.txt", true);
         } catch (IOException e) {
-            printMessageToConsole("ERROR", "Could not open connection to file. Only printing logs to console.");
+            //printMessageToConsole("ERROR", "Could not open connection to file. Only printing logs to console.");
         }
         this.logWriter = writer;
     }
+    public static CustomLogger getLogger(boolean printToConsole) {
+        // CustomLogger will not be created until it is needed
+        if(logger == null) {
+            logger = new CustomLogger(printToConsole);
+        }
+        return logger;
+    }
+    /* the formatMessage method returns a string with the type of log aka "level" in brackets [],
+    followed by the message which is also parameter of this method, and the time that it occurred  */
+    private String formatMessage(String level, String message) {
+        return String.format("[%s] %s at %s", level, message, LocalDateTime.now());
+    }
+    //The logMessageToFile method will write the formattedMessage into resources/log.txt
+    private void logMessageToFile(String formattedMessage) {
+        if (logWriter != null) {
+            try {
+                logWriter.write(formattedMessage + "\n");
+                logWriter.flush();
+            } catch (IOException e) {
+                printMessageToConsole("ERROR", "Could not write message to file");
+            }
+        }
+    }
+    // printMessageToConsole method will print the message parameter into console with type of log aka "level" with the corresponding color
+    private void printMessageToConsole(String level, String message) {
+        switch (level) {
+            case "INFO":
+                System.out.println(ANSI_GREEN + message + ANSI_RESET);//always reset at end so the last color choice does not remain.
+                break;
+            case "WARN":
+                System.out.println(ANSI_PURPLE + message + ANSI_RESET);
+                break;
+            case "ERROR": // to be filled out later
+            case "FATAL":
+                System.out.println(ANSI_RED + message + ANSI_RESET);
+                break;
+        }
 
+    }
+    /*the following methods [info,warn,error,fatal] will log and print to console the activity that occurs as the program is running
+    when they are called
+     */
+    public void info(String message, Object...extra) { // the Object... extra is in case we want to pass multiple values inside our message when we format
+        String formattedMessage = formatMessage("INFO", String.format(message, extra)); // creates a string with the message provided, the extra is for a case like String.format("[%s] %s at %s", level, message, LocalDateTime.now())
+        logMessageToFile(formattedMessage);//actually logs in the message to the log.txt file
+        if(printToConsole) printMessageToConsole("INFO", formattedMessage); //if printToConsole is True, also put the same formattedMessage into console
+    }
+    public void warn(String message, Object...extra) {
+        String formattedMessage = formatMessage("WARN", String.format(message, extra));
+        logMessageToFile(formattedMessage);
+        if(printToConsole) printMessageToConsole("WARN", formattedMessage);
+    }
+    public void error(String message, Object...extra) {
+        String formattedMessage = formatMessage("ERROR", String.format(message, extra));
+        logMessageToFile(formattedMessage);
+        if(printToConsole) printMessageToConsole("ERROR", formattedMessage);
+    }
+    public void fatal(String message, Object...extra) {
+        String formattedMessage = formatMessage("FATAL", String.format(message, extra));
+        logMessageToFile(formattedMessage);
+        if(printToConsole) printMessageToConsole("FATAL", formattedMessage);
+    }
 }
